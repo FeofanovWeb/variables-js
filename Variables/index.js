4610c570f46ad66aad57e056e1da6e93b4b62001
FeofanovWeb <117527824+FeofanovWeb@users.noreply.github.com>
2022-11-10 23:27:57 +1000

    js_variables

---
+++ Variables/index.js
@@ -0,0 +1,45 @@
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...когда нам нужно выбрать цвет
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+
+let user = 'John';   //str
+let age = 24;   // numder
+let message = 2.4; // float
+let message1;
+
+let a = "Hello World!"
+
+let b;
+
+b = a;
+
+let user4 = 'John',
+  age6 = 25,
+  message2 = 'Hello';
+
+alert(user);
+alert(age);
+alert(message); //alert выводит на экран
+
+alert(a);
+alert(b);
+
+message = 'Hello!';
+
+message = 'World!'; // значение изменено
+
+// повторение ключевого слова 'let' приводит к ошибке
+
+alert(message);
+
+// let 1a; // не может начинаться с цифры
+
+// let my-name; // дефис '-' не разрешён в имени
+
+
